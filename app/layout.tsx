729995ac9cca2c707e85e02b8d9f43b3df600728
matellorson <snr.mn.lordson@gmail.com>
2025-05-17 16:07:49 +0100

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,12 @@ import "./globals.css";
 import { createClient } from "@/utils/supabase/server";
 import { redirect } from "next/navigation";
 
+const defaultUrl = process.env.VERCEL_URL
+  ? `https://${process.env.VERCEL_URL}`
+  : "http://localhost:3000";
+
 export const metadata = {
+  metadataBase: new URL(defaultUrl),
   title: "xvest",
   description: "",
 };
