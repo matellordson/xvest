db8c5d005f10795696d4bdcced63c25f968797e6
matellorson <snr.mn.lordson@gmail.com>
2025-05-17 13:16:33 +0100

    changed protected to dashbaord

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,7 +2,7 @@ import { createClient } from "@/utils/supabase/server";
 import { InfoIcon } from "lucide-react";
 import { redirect } from "next/navigation";
 
-export default async function ProtectedPage() {
+export default async function dashbaordPage() {
   const supabase = await createClient();
 
   const {
@@ -18,7 +18,7 @@ export default async function ProtectedPage() {
       <div className="w-full">
         <div className="bg-accent text-sm p-3 px-5 rounded-md text-foreground flex gap-3 items-center">
           <InfoIcon size="16" strokeWidth={2} />
-          This is a protected page that you can only see as an authenticated
+          This is a dashbaord page that you can only see as an authenticated
           user
         </div>
       </div>
