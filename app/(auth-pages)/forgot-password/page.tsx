dad28c2ecb95a71993ae63b7545bd15613590ce4
matellorson <snr.mn.lordson@gmail.com>
2025-05-21 22:19:25 +0100

    tweak forgot password

--- app/(auth-pages)/forgot-password/page.tsx
+++ app/(auth-pages)/forgot-password/page.tsx
@@ -5,33 +5,49 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import Link from "next/link";
 import { SmtpMessage } from "../smtp-message";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 export default async function ForgotPassword(props: {
   searchParams: Promise<Message>;
 }) {
   const searchParams = await props.searchParams;
   return (
-    <>
-      <form className="flex-1 flex flex-col w-full gap-2 text-foreground [&>input]:mb-6 min-w-64 max-w-64 mx-auto">
-        <div>
-          <h1 className="text-2xl font-medium">Reset Password</h1>
-          <p className="text-sm text-secondary-foreground">
+    <div className="mx-auto mt-32 max-w-sm">
+      <Card>
+        <CardHeader>
+          <CardTitle>Reset Password</CardTitle>
+          <CardDescription>
             Already have an account?{" "}
             <Link className="text-primary underline" href="/sign-in">
               Sign in
             </Link>
-          </p>
-        </div>
-        <div className="flex flex-col gap-2 [&>input]:mb-3 mt-8">
-          <Label htmlFor="email">Email</Label>
-          <Input name="email" placeholder="you@example.com" required />
-          <SubmitButton formAction={forgotPasswordAction}>
-            Reset Password
-          </SubmitButton>
-          <FormMessage message={searchParams} />
-        </div>
-      </form>
-      <SmtpMessage />
-    </>
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form className="mx-auto flex w-full min-w-64 max-w-64 flex-1 flex-col gap-2 text-foreground [&>input]:mb-6">
+            <div className="flex flex-col gap-2 [&>input]:mb-3">
+              <Label htmlFor="email">Email</Label>
+              <Input
+                name="email"
+                placeholder="you@example.com"
+                type="email"
+                required
+              />
+              <SubmitButton formAction={forgotPasswordAction}>
+                Reset Password
+              </SubmitButton>
+              <FormMessage message={searchParams} />
+            </div>
+          </form>
+          {/* <SmtpMessage /> */}
+        </CardContent>
+      </Card>
+    </div>
   );
 }
