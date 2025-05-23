0d57293a86f5317dd59ee2ac0ecfb6fed97170c9
matellorson <snr.mn.lordson@gmail.com>
2025-05-23 14:36:40 +0100

    cmt

--- app/protected/dashboard/portfolio/(tesla-components)/tesla-models.tsx
+++ app/protected/dashboard/portfolio/(tesla-components)/tesla-models.tsx
@@ -16,92 +16,95 @@ export default async function TeslaModels() {
       <div className="flex items-center justify-between">
         <p className="text-xl font-semibold tracking-tight">Models</p>
       </div>
-      {teslaModel?.map((data) => (
-        <Link
-          href={`/protected/dashboard/portfolio/invest/${data.slug}`}
-          key={data.model}
-          className="group"
-        >
-          <div className="flex items-center justify-between rounded-lg border bg-skin px-3 py-2 transition-colors group-hover:bg-secondary">
-            {/* left */}
-            <div className="flex w-fit items-center justify-between gap-x-2">
-              <div className="">
-                <p className="text-lg">{data.model}</p>
-                <p className="text-sm capitalize text-muted-foreground">
-                  {data.type}
-                </p>
+      <div className="divide-y">
+        {teslaModel?.map((data) => (
+          <div className="" key={data.model}>
+            <Link
+              href={`/protected/dashboard/tesla/invest/${data.slug}`}
+              className="group"
+            >
+              <div className="flex items-center justify-between rounded px-3 py-2 transition-colors hover:shadow-lg group-hover:bg-muted dark:shadow-black">
+                {/* left */}
+                <div className="flex w-fit items-center justify-between gap-x-2">
+                  <div className="">
+                    <p className="text-lg">{data.model}</p>
+                    <p className="text-sm capitalize text-muted-foreground">
+                      {data.type}
+                    </p>
+                  </div>
+                </div>
+                {/* ceneter */}
+                <div className="absolute left-6 mx-auto flex w-[88vw] items-center justify-center">
+                  {data.model == "Cyber Truck" ? (
+                    <Image
+                      src={"/model-showcase/cyber-truck.png"}
+                      alt={"cyber-truck"}
+                      priority
+                      width={100}
+                      height={100}
+                      className="w-40 dark:opacity-80"
+                    />
+                  ) : data.model == "Model 3" ? (
+                    <Image
+                      src={"/model-showcase/model-3.png"}
+                      alt={"model-3"}
+                      priority
+                      width={100}
+                      height={100}
+                      className="w-40 dark:opacity-80"
+                    />
+                  ) : data.model == "Model S" ? (
+                    <Image
+                      src={"/model-showcase/model-s.png"}
+                      alt={"model-s"}
+                      priority
+                      width={100}
+                      height={100}
+                      className="w-40 dark:opacity-80"
+                    />
+                  ) : data.model == "Model X" ? (
+                    <Image
+                      src={"/model-showcase/model-x.png"}
+                      alt={"model-x"}
+                      priority
+                      width={100}
+                      height={100}
+                      className="w-40 dark:opacity-80"
+                    />
+                  ) : data.model == "Model Y" ? (
+                    <Image
+                      src={"/model-showcase/model-y.png"}
+                      alt={"model-y"}
+                      priority
+                      width={100}
+                      height={100}
+                      className="w-40 dark:opacity-80"
+                    />
+                  ) : (
+                    ""
+                  )}
+                </div>
+                {/* right */}
+                <div className="flex flex-col items-end justify-between gap-y-1">
+                  <p className="font-semibold tracking-wide">
+                    $
+                    {data.price.toLocaleString("en-US", {
+                      maximumFractionDigits: 2,
+                    })}
+                  </p>
+                  <p className="text-sm tracking-wide text-muted-foreground">
+                    $
+                    {data.plan1.toLocaleString("en-US", {
+                      maximumFractionDigits: 2,
+                    })}
+                    <span className="text-xs">/mo</span>
+                  </p>
+                </div>
               </div>
-            </div>
-            {/* ceneter */}
-            <div className="absolute left-6 mx-auto flex w-[88vw] items-center justify-center">
-              {data.model == "Cyber Truck" ? (
-                <Image
-                  src={"/model-showcase/cyber-truck.png"}
-                  alt={"cyber-truck"}
-                  priority
-                  width={100}
-                  height={100}
-                  className="w-40 dark:opacity-80"
-                />
-              ) : data.model == "Model 3" ? (
-                <Image
-                  src={"/model-showcase/model-3.png"}
-                  alt={"model-3"}
-                  priority
-                  width={100}
-                  height={100}
-                  className="w-40 dark:opacity-80"
-                />
-              ) : data.model == "Model S" ? (
-                <Image
-                  src={"/model-showcase/model-s.png"}
-                  alt={"model-s"}
-                  priority
-                  width={100}
-                  height={100}
-                  className="w-40 dark:opacity-80"
-                />
-              ) : data.model == "Model X" ? (
-                <Image
-                  src={"/model-showcase/model-x.png"}
-                  alt={"model-x"}
-                  priority
-                  width={100}
-                  height={100}
-                  className="w-40 dark:opacity-80"
-                />
-              ) : data.model == "Model Y" ? (
-                <Image
-                  src={"/model-showcase/model-y.png"}
-                  alt={"model-y"}
-                  priority
-                  width={100}
-                  height={100}
-                  className="w-40 dark:opacity-80"
-                />
-              ) : (
-                ""
-              )}
-            </div>
-            {/* right */}
-            <div className="flex flex-col items-end justify-between gap-y-1">
-              <p className="font-semibold tracking-wide">
-                $
-                {data.price.toLocaleString("en-US", {
-                  maximumFractionDigits: 2,
-                })}
-              </p>
-              <p className="text-sm tracking-wide text-muted-foreground">
-                $
-                {data.plan1.toLocaleString("en-US", {
-                  maximumFractionDigits: 2,
-                })}
-                <span className="text-xs">/mo</span>
-              </p>
-            </div>
+            </Link>
           </div>
-        </Link>
-      ))}
+        ))}
+      </div>
     </div>
   );
 }
