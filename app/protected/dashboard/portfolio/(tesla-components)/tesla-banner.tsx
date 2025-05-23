b896e50b85974c920575339fb24cfd730a25ca16
matellorson <snr.mn.lordson@gmail.com>
2025-05-23 14:17:51 +0100

    tweak

--- app/protected/dashboard/portfolio/(tesla-components)/tesla-banner.tsx
+++ app/protected/dashboard/portfolio/(tesla-components)/tesla-banner.tsx
@@ -16,18 +16,32 @@ export default async function TeslaBanner() {
     .reduce((sum, price) => sum + price, 0);
 
   return (
-    <div className="h-44 w-full rounded-lg border">
+    <div className="flex h-44 w-full justify-between rounded-lg border">
       <div className="space-y-4">
         <div>
           <Image
-            src="/logo/tesla-gallery-logos/07_0x0-Tesla_Wordmark_20_White.png"
+            src="/logo/tesla-gallery-logos/04_0x0-Tesla_Wordmark_10_White.png"
             alt="tesla logo"
             width={150}
             height={150}
-            className="ml-1 mt-1"
+            className={`ml-1 mt-1 ${"hidden dark:block"}`}
+            priority
+          />
+          <Image
+            src="/logo/tesla-gallery-logos/08_0x0-Tesla_Wordmark_10_Black.png"
+            alt="tesla logo"
+            width={150}
+            height={150}
+            className={`ml-1 mt-1 ${"dark:hidden"}`}
+            priority
           />
         </div>
         <div className="space-y-1 px-4">
+          <div className="">
+            <p className="text-xs tracking-wide text-muted-foreground">
+              Total Investment
+            </p>
+          </div>
           <p className="text-4xl font-semibold tracking-wide">
             ${totalPaid.toLocaleString("en-US", { maximumFractionDigits: 2 })}
           </p>
