99bf8239d142f8410de97297d1dde9afdea3da78
matellorson <snr.mn.lordson@gmail.com>
2025-05-23 01:44:00 +0100

    restricted admin from going to cleint page

---
+++ app/protected/dashboard/portfolio/layout.tsx
@@ -0,0 +1,18 @@
+import { createClient } from "@/utils/supabase/server";
+import { redirect } from "next/navigation";
+
+export default async function PortfolioLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const supabase = await createClient();
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+  if (user?.id == process.env.ADMIN_ID) {
+    return redirect("/protected/dashboard");
+  }
+  return <div className="">{children}</div>;
+}
